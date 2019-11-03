f81e4266300261fe2c2014847d7592b9e282ad59
LekaAli <atshoane@gmail.com>
2019-11-03 19:57:38 +0200

    logic change for calculations

--- products/models.py
+++ products/models.py
@@ -79,10 +79,10 @@ class GrossProfit(models.Model):
 
     def save(self, *args, **kwargs):
         from revenues.models import ProductRevenue
-        product_revenue = ProductRevenue.objects.filter(product_id=self.product.id, financial_year_id=self.financial_year.id, month=self.month)
+        product_revenue = ProductRevenue.objects.filter(financial_year_id=self.financial_year.id, month=self.month)
         if product_revenue.count() > 0:
             revenue = product_revenue.first()
-            self.gross_profit_value = revenue.product_revenue - self.cost_of_sale
+            self.gross_profit_value = revenue.product_revenue - (self.cost_of_sale / float(100) * revenue.product_revenue)
         else:
             self.gross_profit_value = 0
         super(GrossProfit, self).save(*args, **kwargs)
@@ -92,13 +92,24 @@ class GrossProfit(models.Model):
 
 
 class Expense(models.Model):
-    # sale = models.ForeignKey(Sale, related_name='sale_expenses', on_delete=models.CASCADE, blank=True, null=True)
-    financial_year = models.ForeignKey(
-        FinancialYear, related_name='expense_f_year', on_delete=models.CASCADE, blank=False, null=True)
-    month = models.PositiveSmallIntegerField(default=1)
+    MONTHS = (
+        (1, 'January'),
+        (2, 'February'),
+        (3, 'March'),
+        (4, 'April'),
+        (5, 'May'),
+        (6, 'June'),
+        (7, 'July'),
+        (8, 'August'),
+        (9, 'September'),
+        (10, 'October'),
+        (11, 'November'),
+        (12, 'December'),
+    )
+    financial_year = models.ForeignKey(FinancialYear, related_name='expense_f_year', on_delete=models.CASCADE, blank=False, null=True)
+    month = models.PositiveSmallIntegerField(choices=MONTHS)
     is_fixed = models.BooleanField()
     value = models.FloatField()
-    total = models.FloatField(default=0.00)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -107,16 +118,6 @@ class Expense(models.Model):
         verbose_name_plural = 'Expenses'
 
     def save(self, *args, **kwargs):
-        from revenues.models import ProductRevenue
-        product_revenues = ProductRevenue.objects.filter(financial_year_id=self.financial_year.id, month=self.month)
-        if product_revenues.count() > 0:
-            self.total = sum([revenue.product_revenue for revenue in product_revenues])
-        else:
-            self.total = 0
-        if self.is_fixed is True:
-            self.total = self.total - self.value
-        else:
-            self.total = self.total - (self.value / float(100) * self.total)
         super(Expense, self).save(*args, **kwargs)
 
     def __str__(self):
@@ -124,9 +125,25 @@ class Expense(models.Model):
 
 
 class ProfitBeforeTax(models.Model):
+    MONTHS = (
+        (1, 'January'),
+        (2, 'February'),
+        (3, 'March'),
+        (4, 'April'),
+        (5, 'May'),
+        (6, 'June'),
+        (7, 'July'),
+        (8, 'August'),
+        (9, 'September'),
+        (10, 'October'),
+        (11, 'November'),
+        (12, 'December'),
+    )
     financial_year = models.ForeignKey(FinancialYear, related_name='profit_before_tax_f_year', on_delete=models.CASCADE, blank=False, null=True)
+    month = models.PositiveSmallIntegerField(choices=MONTHS, blank=True, null=True)
     gross = models.FloatField(default=0.00)
     expense = models.FloatField(default=0.00)
+    monthly_gross_value = models.FloatField(default=0.00)
     total_gross_value = models.FloatField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
@@ -136,17 +153,18 @@ class ProfitBeforeTax(models.Model):
         verbose_name_plural = 'Profit Before Taxes'
 
     def save(self, *args, **kwargs):
-        gross_values = GrossProfit.objects.filter(financial_year=self.financial_year.id)
-        expenses = Expense.objects.filter(financial_year=self.financial_year.id)
-        if expenses.count() > 0:
-            self.expense = sum([expense.total for expense in expenses])
-        else:
-            self.expense = 0
+        gross_values = GrossProfit.objects.filter(financial_year=self.financial_year.id, month=self.month)
         if gross_values.count() > 0:
-            self.gross_value = sum([gross.gross_profit_value for gross in gross_values])
+            gross_value = sum([gross.gross_profit_value for gross in gross_values])
+            expenses = Expense.objects.filter(month=self.month)
+
+            fixed_expenses = sum([expense.value if expenses.financial_year == self.financial_year else expense.value + (self.financial_year.inflation / float(100) * expense.value) for expense in expenses if expense.is_fixed is True])
+            not_fixed_expenses = sum([(expense.value / float(100) * gross_value) for expense in expenses if expense.is_fixed is False])
+
+            self.monthly_gross_value = gross_value - (fixed_expenses + not_fixed_expenses)
+            self.total_gross_value = self.total_gross_value + self.monthly_gross_value
         else:
-            self.gross_value = 0
-        self.total_gross_value = self.gross_value - self.expense
+            self.monthly_gross_value = 0
         super(ProfitBeforeTax, self).save(*args, **kwargs)
 
     def __str__(self):
@@ -154,10 +172,10 @@ class ProfitBeforeTax(models.Model):
 
 
 class Tax(models.Model):
-    profit_before_tax = models.ForeignKey(ProfitBeforeTax, related_name='profit_before_tax', on_delete=models.CASCADE, blank=True, null=True)
-    total_tax_value = models.FloatField(default=0.00)
+    financial_year = models.ForeignKey(FinancialYear, related_name='tax_f_year', on_delete=models.CASCADE, blank=False, null=True)
     tax_percentage = models.FloatField(default=1.00)
-    profit_loss_value = models.FloatField()
+    profit_loss_value = models.FloatField(default=0.00)
+    total_tax_value = models.FloatField(default=0.00)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -166,30 +184,41 @@ class Tax(models.Model):
         verbose_name_plural = 'Taxes'
 
     def save(self, *args, **kwargs):
-        super(Tax, self).save(*args, **kwargs)
-        if self.profit_before_tax.total_gross_value <= 0:
+        profit_before_tax = ProfitBeforeTax.objects.filter(financial_year=self.financial_year.id)
+        if profit_before_tax.count() > 0:
+            total_gross_value = sum([p_before_tax.total_gross_value for p_before_tax in profit_before_tax])
+        else:
+            total_gross_value = 0.00
+
+        if total_gross_value <= 0:
             self.total_tax_value = 0
+            self.profit_loss_value = abs(total_gross_value)
         else:
             # calculate tax
-            if self.profit_loss_value == 0:
-                self.total_tax_value = self.profit_before_tax.total_gross_value(self.tax_percentage / float(100))
-            else:
-                # cover loss for previous years
-                profit_after_loss_deduction = self.profit_before_tax.total_gross_value - self.profit_loss_value
-                if profit_after_loss_deduction > 0:
-                    self.profit_loss_value = 0
-                    self.total_tax_value = profit_after_loss_deduction(self.tax_percentage / float(100))
+            previous_taxes = Tax.objects.all()
+            if previous_taxes.count() > 0:
+                previous_tax = previous_taxes.lastest()
+                if previous_tax.profit_loss_value == 0:
+                    self.total_tax_value = total_gross_value * self.tax_percentage / float(100)
                 else:
-                    self.profit_loss_value = abs(profit_after_loss_deduction)
-                    self.total_tax_value = 0
+                    # cover loss for previous years
+                    profit_after_loss_deduction = total_gross_value - previous_tax.profit_loss_value
+                    if profit_after_loss_deduction > 0:
+                        self.profit_loss_value = 0
+                        self.total_tax_value = profit_after_loss_deduction * self.tax_percentage / float(100)
+                    else:
+                        self.profit_loss_value = abs(profit_after_loss_deduction)
+                        self.total_tax_value = 0
+            self.total_tax_value = total_gross_value * self.tax_percentage / float(100)
+        super(Tax, self).save(*args, **kwargs)
 
     def __str__(self):
         return 'Total tax payable: %s' % self.total_tax_value
 
 
 class NetProfit(models.Model):
-    tax = models.ForeignKey(Tax, related_name='tax', on_delete=models.CASCADE, blank=True, null=True)
-    net_profit = models.FloatField()
+    financial_year = models.ForeignKey(FinancialYear, related_name='net_profit_f_year', on_delete=models.CASCADE, blank=False, null=True)
+    net_profit = models.FloatField(default=0.00)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -198,8 +227,17 @@ class NetProfit(models.Model):
         verbose_name_plural = 'Net Profits'
 
     def save(self, *args, **kwargs):
+        taxes = Tax.objects.filter(financial_year=self.financial_year.id)
+        profits_before_tax = ProfitBeforeTax.objects.filter(financial_year=self.financial_year.id)
+        if profits_before_tax.count() > 0:
+            profit_before_tax_value = sum([p_before_tax.total_gross_value for p_before_tax in profits_before_tax])
+            if taxes.count() > 0:
+                self.net_profit = profit_before_tax_value - taxes.first().total_tax_value
+        else:
+            profit_before_tax_value = 0
+            self.net_profit = profit_before_tax_value
+
         super(NetProfit, self).save(*args, **kwargs)
-        self.net_profit = self.tax.profit_before_tax.total_value - self.tax.total_tax_value
 
     def __str__(self):
         return 'Net Profit: %s' % self.net_profit
