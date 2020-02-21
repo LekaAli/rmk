410c6d33619deae5c21b8f1f7adeee4642876211
LekaAli <atshoane@gmail.com>
2020-02-21 07:23:34 +0200

    report layout resolved

--- revenues/__init__.py
+++ revenues/__init__.py
@@ -7,14 +7,31 @@ class AppEngine(object):
     def generate_report_data(cls):
         data = dict()
         from revenues.models import Revenue, Sale
+        month_dict = {key: value for key, value in sorted(Revenue.MONTHS, key=lambda x: x[0])}
         products = list(Sale.objects.values_list('product', flat=True).distinct())
         revenues = Revenue.objects.filter(product_id__in=products).order_by('id')
-        product_revenues = revenues.values_list('id', 'product__name', 'financial_year', 'month', 'product_revenue')
+        product_revenues = revenues.values_list(
+            'id',
+            'product__name',
+            'financial_year__description',
+            'month',
+            'product_revenue'
+        )
         sorted_product_revenues = sorted(product_revenues, key=lambda x: x[1])
         grouped_product_revenues = groupby(sorted_product_revenues, key=lambda x: x[1])
         groups = dict()
         for product_name, product_revenues in grouped_product_revenues:
-            groups[product_name] = list(product_revenues)
+            processed_product_revenues = list()
+            for product_revenue in product_revenues:
+                product_revenue = list(product_revenue)
+                product_revenue[3] = month_dict.get(product_revenue[3])[:3]
+                processed_product_revenues.append(product_revenue)
+            processed_product_revenues = groupby(processed_product_revenues, key=lambda x:x[2])
+            processed_product = dict()
+            for year, revenue in processed_product_revenues:
+                processed_product[year] = list(revenue)
+            groups[product_name] = processed_product
+        
         data['revenues'] = groups
         product_sales = Sale.objects.filter(product_id__in=products).values_list(
             'product__name',
