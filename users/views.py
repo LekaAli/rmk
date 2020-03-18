707d07191850251426684e718afe2148f23e1679
LekaAli <atshoane@gmail.com>
2020-03-18 08:08:36 +0200

    syntax error

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created, you can now login')
+            messages.success(request, 'Your account has been created, you can now login')
             return redirect('login')
     else:
         form = UserRegisterForm()
